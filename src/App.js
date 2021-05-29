4af518d29b87a3ade171f146f8c1a5d7d679b5e9
David Liu <david@Davids-MacBook-Pro.local>
2021-05-28 21:32:20 -0700

    disabled fetching data from backend. Switched to local data

--- src/App.js
+++ src/App.js
@@ -8,31 +8,38 @@ function App() {
   const [data, setData] = useState([])
 
   useEffect(()=> {
-    const getData = async () => {
-      const menuData = await fetchData("menu")
-      setData(menuData)
-    }
+    // Fetches data from backend
+    // const getData = async () => {
+    //   const menuData = await fetchData("menu")
+    //   setData(menuData)
+    //   console.log("fetch server")
+    //   console.log(menuData)
+    // }
 
-    getData()
+    // getData()
+    
+    // Using local data
+    var data = require('./menu.json')
+    setData( data )
+
   }, [])
 
-  // Fetch data
-  const fetchData = async (category) => {
-    const res = await fetch ('http://localhost:5000/' + category)
-    const fetchData = await res.json()
+  // // Fetch data
+  // const fetchData = async (category) => {
+  //   const res = await fetch ('http://localhost:5000/' + category)
+  //   const fetchData = await res.json()
     
-    // console.log(fetchData)
-    return fetchData
-  }
+  //   return fetchData
+  // }
 
   const addOrder = async (order) => {
-    await fetch('http://localhost:5000/orders', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(order),
-    })
+    // await fetch('http://localhost:5000/orders', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify(order),
+    // })
     alert("Your order is confirmed.")
   }
 
